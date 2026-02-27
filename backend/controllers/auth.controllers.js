dc4676bd0792f8376c489500284565ce50a636af
AVIS10060 <avisharmaid@gmail.com>
2026-02-27 15:17:27 +0530

    added a comment

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -91,7 +91,7 @@ export const signIn = async(req,res)=>{
 export const signOut = (req, res) => {
     try {
         res.clearCookie("token");
-        return res.status(200).json({ message: "Logged out successfully" });
+        return res.status(200).json({ message: "Logged out successfully!!" });
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
