e91a39739132140be4b47242ef14770067b68fc1
AVIS10060 <avisharmaid@gmail.com>
2026-02-28 21:04:16 +0530

    forgot-password-route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes ,Route} from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import ForgotPassword from './pages/ForgotPassword'
 
 export const serverUrl = 'http://localhost:8000'
 
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path = '/signup' element = {<SignUp></SignUp>}></Route>
       <Route path = '/signin' element = {<SignIn></SignIn>}></Route>
+      <Route path = '/forgot-password' element = {<ForgotPassword></ForgotPassword>}></Route>
     </Routes>
   )
 }
