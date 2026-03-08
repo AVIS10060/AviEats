5abcb0a01dc9df23c1e5678e3748eab5dd0fd310
AVIS10060 <avisharmaid@gmail.com>
2026-03-08 23:01:20 +0530

    added shop router and item router

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.routes.js'
 import cors from 'cors'
 import userRouter from './routes/user.routes.js'
+import shopRouter from './routes/shop.routes.js'
+import itemRouter from './routes/item.route.js'
 
 
 dotenv.config()
@@ -16,12 +18,15 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
       origin : "http://localhost:5173",
-    credentials:true,
-} 
+      credentials:true,
+    } 
 ))
+app.use("/public", express.static("public"))
 
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+app.use("/api/shop",shopRouter)
+app.use("/api/item",itemRouter)
 
 app.listen(port,()=>{
     connectDB()
