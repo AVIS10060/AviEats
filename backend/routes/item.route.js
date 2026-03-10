44be39c522b82410661f80cfd7b7ecba2160f076
AVIS10060 <avisharmaid@gmail.com>
2026-03-10 08:36:26 +0530

    get item by id

--- backend/routes/item.route.js
+++ backend/routes/item.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import isAuth from '../middlewares/isAuth.js'
-import {  createItem, ediItem } from '../controllers/item.controllers.js'
+import {  createItem, ediItem, getItemById } from '../controllers/item.controllers.js'
 import { upload } from '../middlewares/multer.js'
 
 const itemRouter = express.Router()
 
-itemRouter.get("/add-item",isAuth,upload.single("image"),createItem)
-itemRouter.get("/edit-item/:itemId",isAuth,upload.single("image"),ediItem)
+itemRouter.post("/add-item",isAuth,upload.single("image"),createItem)
+itemRouter.post("/edit-item/:itemId",isAuth,upload.single("image"),ediItem)
+itemRouter.get("/get-by-id/:itemId",isAuth,getItemById)
 
 export default itemRouter
 
