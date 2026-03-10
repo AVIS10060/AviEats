248630b5272449c896bb6f8261bc53bc4b87a964
AVIS10060 <avisharmaid@gmail.com>
2026-03-10 08:37:47 +0530

    added forgot password

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { FaArrowLeft } from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
+import { serverUrl } from '../App';
+import toast from "react-hot-toast";
+import axios from 'axios';
+
 
   const primaryColor = "#ff4d2d";
   const hoverColor = "#e64323";
@@ -9,12 +13,61 @@ import { useNavigate } from 'react-router-dom';
 
 const ForgotPassword = () => {
 
-    const [step,setStep] = useState(3)
-    const [otp,setOtp] = useState()
-    const [newPassword,setNewPassword] = useState()
-    const [confirmPassword,setConfirmPassword] = useState()
-
+    const [step,setStep] = useState(1)
+    const [email,setEmail] = useState("")
+    const [otp,setOtp] = useState("")
+    const [newPassword,setNewPassword] = useState("")
+    const [confirmPassword,setConfirmPassword] = useState("")
     const navigate = useNavigate()
+
+
+    const handleSendOtp = async() =>{
+        try {
+            const response = await axios.post(`${serverUrl}/api/auth/send-otp`,{
+                email
+            },{ withCredentials: true })
+            console.log(response)
+            setStep(2)
+            toast.success("otp sent")
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+    }
+    const handleVerifyOtp = async() =>{
+        try {
+            const response = await axios.post(`${serverUrl}/api/auth/verify-otp`,{
+                email,otp
+            },{ withCredentials: true })
+            console.log(response)
+            setStep(3)
+            toast.success(response.data.message)
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+    }
+    const handleResetPassword = async() =>{
+        if(newPassword !== confirmPassword){
+            return console.log("password are not same")
+        }
+        try {
+            const response = await axios.post(`${serverUrl}/api/auth/reset-password`,{
+                email,newPassword,confirmPassword
+            },{ withCredentials: true })
+            console.log(response)
+            toast.success(response.data.message)
+            navigate("/signin")
+        } catch (error) {
+            console.log(error)
+            
+        }
+    }
+
+
+
   return (
     <div className='h-screen w-screen flex items-center justify-center bg-pink-100'>
        <div className='h-64 max-w-2xl w-full flex flex-col gap-5 items-center justify-center bg-white '> 
@@ -37,8 +90,8 @@ const ForgotPassword = () => {
               name="email"
               placeholder="Enter your email"
               className="w-full border rounded-lg px-4 py-2 focus:outline-none focus:ring-2"
-            //   value={formData.email}
-            //   onChange={handleChange}
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
               required
             />
             <button
@@ -51,6 +104,7 @@ const ForgotPassword = () => {
             onMouseOut={(e) =>
               (e.currentTarget.style.backgroundColor = primaryColor)
             }
+            onClick={handleSendOtp}
           >
            Send Otp
           </button>
@@ -73,6 +127,7 @@ const ForgotPassword = () => {
               required
             />
             <button
+            onClick={handleVerifyOtp}
             type="submit"
             className="w-full text-white font-semibold py-2 rounded-lg transition duration-200 disabled:opacity-60"
             style={{ backgroundColor: primaryColor }}
@@ -117,6 +172,7 @@ const ForgotPassword = () => {
               required
             />
             <button
+            onClick={handleResetPassword}
             type="submit"
             className="w-full text-white font-semibold py-2 rounded-lg transition duration-200 disabled:opacity-60"
             style={{ backgroundColor: primaryColor }}
