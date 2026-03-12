6cb43b3b33f62cafca1d8e8ef155c9890783e2ac
AVIS10060 <avisharmaid@gmail.com>
2026-03-12 12:16:45 +0530

    updated routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,72 @@
-import React from 'react'
-import { Routes ,Route} from 'react-router-dom'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import ForgotPassword from './pages/ForgotPassword'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import { Toaster } from "react-hot-toast";
+import useGetCurrentUser from "./hooks/useGetCurrentUser";
+import { useSelector } from "react-redux";
+import Home from "./pages/Home";
+import Navbar from "./components/NavBar";
+import useGetCity from "./hooks/useGetCity";
+import useGetMyShop from "./hooks/useGetMyShop";
+import CreateEditShop from "./pages/CreateEditShop";
+import AddItem from "./components/AddItem";
+import EditItem from "./components/EditItem";
 
-export const serverUrl = 'http://localhost:8000'
+export const serverUrl = "http://localhost:8000";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path = '/signup' element = {<SignUp></SignUp>}></Route>
-      <Route path = '/signin' element = {<SignIn></SignIn>}></Route>
-      <Route path = '/forgot-password' element = {<ForgotPassword></ForgotPassword>}></Route>
-    </Routes>
-  )
-}
 
-export default App
+  useGetCurrentUser();
+  useGetCity()
+  useGetMyShop()
+
+  const { userData } = useSelector((state) => state.user);
+
+  return (
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+
+      <Routes>
+
+        {/* Public routes */}
+        <Route
+          path="/signup"
+          element={userData ? <Navigate to="/" /> : <SignUp />}
+        />
+
+        <Route
+          path="/signin"
+          element={userData ? <Navigate to="/" /> : <SignIn />}
+        />
+
+        <Route
+          path="/forgot-password"
+          element={<ForgotPassword />}
+        />
+
+        {/* Protected route */}
+        <Route
+          path="/"
+          element={userData ? <Home /> : <Navigate to="/signin" />}
+        />
+         <Route
+          path="/create-edit-shop"
+          element={userData ?<CreateEditShop /> : <Navigate to="/signin" />}
+        />
+         <Route
+          path="/add-food"
+          element={userData && <AddItem />}
+        />
+         <Route
+          path="/edit-item/:itemId"
+          element={userData && <EditItem />}
+        />
+
+      </Routes>
+    </>
+  );
+};
+
+export default App;
