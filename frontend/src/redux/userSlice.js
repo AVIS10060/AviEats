cc2f12d9a693cc6a8408c6121fe7918c46bcc7f2
AVIS10060 <avisharmaid@gmail.com>
2026-03-12 12:17:02 +0530

    updated userSlice

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,25 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
     name: "user",
     initialState:{
         userData:null,
-        city:null
+        currentCity:null,
+        currentState:null,
+        currentAddress:null
 
     },
     reducers:{
-        setUserData: (state,action)=>{
-        state.userData = action.payload
+        setUserData: (State,action)=>{
+        State.userData = action.payload
 
         },
-        setCity: (state,action)=>{
-        state.city = action.payload
+        setCurrentCity: (State,action)=>{
+        State.currentCity = action.payload
+
+        },
+        setCurrentState: (State,action)=>{
+        State.currentState = action.payload
+
+        },
+        setCurrentAddress: (State,action)=>{
+        State.currentAddress = action.payload
 
         }
     }
 })
 
 export const  {setUserData}= userSlice.actions
-export const  {setCity}= userSlice.actions
+export const  {setCurrentCity}= userSlice.actions
+export const  {setCurrentState}= userSlice.actions
+export const  {setCurrentAddress}= userSlice.actions
 
 export default userSlice.reducer
