aa22dd94d5a2678c73c58009826aa15e30bb7865
AVIS10060 <avisharmaid@gmail.com>
2026-03-16 12:31:43 +0530

    solve issue in ownerslice

--- frontend/src/redux/ownerSlice.js
+++ frontend/src/redux/ownerSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const ownerSlice = createSlice({
     name: "owner",
     initialState:{
-        myshopData:null,
+        myShopData:null,
         
     },
     reducers:{
