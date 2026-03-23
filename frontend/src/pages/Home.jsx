00844bdb9954194c7a1c735ec9f6b3b660eab5e1
AVIS10060 <avisharmaid@gmail.com>
2026-03-24 00:19:47 +0530

    styling in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,16 +3,22 @@ import { useSelector } from 'react-redux'
 import UserDashboard from '../components/user/UserDashboard'
 import OwnerDashboard from '../components/OwnerDashboard'
 import DeliveryBoyDashboard from '../components/DeliveryBoyDashboard'
+import Navbar from '../components/NavBar'
 
 const Home = () => {
     const {userData} = useSelector(state => state.user)
   return (
-    <div className='w-[100vw] min-h-[100vh]  flex flex-col item-center'>
+  <>
+  <div>
+  <Navbar></Navbar>
+  </div>
+  <div className='w-[100vw] h-full  flex flex-col item-center'>
         {userData.role == "user" && <UserDashboard></UserDashboard>}
         {userData.role == "owner" && <OwnerDashboard></OwnerDashboard>}
         {userData.role == "deliveryboy" && <DeliveryBoyDashboard></DeliveryBoyDashboard>}
 
-    </div>
+  </div>
+  </>
   )
 }
 
