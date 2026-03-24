b9bddfd7a0662690f21b3006d09cd71310f61821
AVIS10060 <avisharmaid@gmail.com>
2026-03-24 23:25:15 +0530

    added some corrections

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,18 @@ import cors from 'cors'
 import userRouter from './routes/user.routes.js'
 import shopRouter from './routes/shop.routes.js'
 import itemRouter from './routes/item.route.js'
+import orderRouter from './routes/order.routes.js'
 
 
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT 
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-      origin : "http://localhost:5173",
+      origin :"http://localhost:5173",
       credentials:true,
     } 
 ))
@@ -27,11 +28,11 @@ app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
 app.use("/api/shop",shopRouter)
 app.use("/api/item",itemRouter)
+app.use("/api/order",orderRouter)
 
-app.listen(port,()=>{
-    connectDB()
+connectDB()
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
-
 })
 
 
