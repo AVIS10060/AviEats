24a6c249a4e0bd55858b9e76bb14e0058294a553
AVIS10060 <avisharmaid@gmail.com>
2026-03-29 00:18:48 +0530

    some changes

--- frontend/src/hooks/useGetCity.jsx
+++ frontend/src/hooks/useGetCity.jsx
@@ -1,42 +1,39 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { setCurrentCity,setCurrentAddress,setCurrentState } from '../redux/userSlice'
-
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setCurrentCity,
+  setCurrentAddress,
+  setCurrentState,
+} from "../redux/userSlice";
+import { setAddress, setLocation } from "../redux/mapSlice";
 
 const useGetCity = () => {
-    const dispatch = useDispatch()
-    const apikey = import.meta.env.VITE_GEOAPIKEY
-    const {userData} = useSelector(state => state.user)
+  const dispatch = useDispatch();
+  const apikey = import.meta.env.VITE_GEOAPIKEY;
+  const { userData } = useSelector((state) => state.user);
 
-    useEffect(()=>{
-        navigator.geolocation.getCurrentPosition(async (position)=>{
-            console.log(position)
-            const latitude = position.coords.latitude
-            const longitude = position.coords.longitude
-            try {
-            const response = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apikey}`)
-            dispatch(setCurrentCity(response?.data?.results[0].city))
-            dispatch(setCurrentState(response?.data?.results[0].state))
-            dispatch(setCurrentAddress(response?.data?.results[0].address_line1))
-            console.log(response.data.results[0].address_line1)
-            console.log(response.data.results[0].state)
-            
-        } catch (error) {
-            console.log(error)
-            
-        }
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      console.log(position);
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      dispatch(setLocation({ lat: latitude, lon: longitude }));
+      try {
+        const response = await axios.get(
+          `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apikey}`,
+        );
+        dispatch(setCurrentCity(response?.data?.results[0].city));
+        dispatch(setCurrentState(response?.data?.results[0].state));
+        dispatch(setCurrentAddress(response?.data?.results[0].address_line1));
+        dispatch(setAddress(response?.data?.results[0].address_line2));
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }, [userData]);
 
-        })
-        
+  return <div></div>;
+};
 
-},[userData])
-
-
-
-  return (
-    <div></div>
-  )
-}
-
-export default useGetCity
+export default useGetCity;
