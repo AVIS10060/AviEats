fb8212f96a8d8c81895c2787411f41e50a9376f7
AVIS10060 <avisharmaid@gmail.com>
2026-03-31 11:32:48 +0530

    token.js

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,8 +1,11 @@
 import JWT from 'jsonwebtoken'
 
-const genToken = async ({userId}) =>{
+const genToken = async (userId) =>{
     try {
-        const token = await JWT.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"})
+        const token =await JWT.sign(
+            { userId: String(userId) }
+            ,process.env.JWT_SECRET,{expiresIn:"7d"})
+         console.log("UserId received in genToken:", userId);
         return token 
     } catch (error) {
         console.log(error)
