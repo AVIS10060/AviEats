a52ea1ec83f0e9f034db1b802129f08c38f66737
AVIS10060 <avisharmaid@gmail.com>
2026-04-21 13:11:07 +0530

    changes

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import isAuth from '../middlewares/isAuth.js'
-import { getMyOrders, placeOrder, updateOrderStatus } from '../controllers/order.controller.js'
+import { acceptOrder, getDeliveryBoyAssignment, getMyOrders, placeOrder, updateOrderStatus } from '../controllers/order.controller.js'
 
 const orderRouter = express.Router()
 
 orderRouter.post("/place-order",isAuth,placeOrder)
 orderRouter.get("/my-orders",isAuth,getMyOrders)
 orderRouter.post("/update-status/:orderId/:shopId",isAuth,updateOrderStatus)
+orderRouter.get("/get-assignments",isAuth,getDeliveryBoyAssignment)
+orderRouter.get("/accept-order/:assignmentId",isAuth,acceptOrder)
 
 
 export default orderRouter
