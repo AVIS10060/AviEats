c5fa67298e8ab708693e7a40291a6bc8b374c3e5
AVIS10060 <avisharmaid@gmail.com>
2026-04-23 12:09:20 +0530

    new routes

--- backend/routes/item.route.js
+++ backend/routes/item.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import isAuth from '../middlewares/isAuth.js'
-import {  createItem, deleteItem, ediItem, getItemByCity, getItemById } from '../controllers/item.controllers.js'
+import {  createItem, deleteItem, ediItem, getItemByCity, getItemById, getItemByShop, searchItems } from '../controllers/item.controllers.js'
 import { upload } from '../middlewares/multer.js'
 
 const itemRouter = express.Router()
@@ -10,6 +10,8 @@ itemRouter.post("/edit-item/:itemId",isAuth,upload.single("image"),ediItem)
 itemRouter.get("/get-by-id/:itemId",isAuth,getItemById)
 itemRouter.get("/delete/:itemId",isAuth,deleteItem)
 itemRouter.get("/get-by-city/:city",isAuth,getItemByCity)
+itemRouter.get("/get-by-shop/:shopId",isAuth,getItemByShop)
+itemRouter.get("/search-items",isAuth,searchItems)
 
 
 export default itemRouter
