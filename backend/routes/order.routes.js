1c92ef17bb050633e0e98447736b632a8e4f54cb
AVIS10060 <avisharmaid@gmail.com>
2026-04-26 00:33:12 +0530

    changes

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import isAuth from '../middlewares/isAuth.js'
-import { acceptOrder, getCurrentOrder, getDeliveryBoyAssignment, getMyOrders, getOrderById, placeOrder, sendDeliveryOtp, updateOrderStatus, verifyDeliveryOtp } from '../controllers/order.controller.js'
+import { acceptOrder, getCurrentOrder, getDeliveryBoyAssignment, getMyOrders, getOrderById, placeOrder, sendDeliveryOtp, updateOrderStatus, verifyDeliveryOtp, verifyPayment } from '../controllers/order.controller.js'
 
 const orderRouter = express.Router()
 
@@ -13,6 +13,8 @@ orderRouter.get("/get-current-order/",isAuth,getCurrentOrder)
 orderRouter.get("/get-order-by-id/:orderId",isAuth,getOrderById)
 orderRouter.post("/send-delivery-otp/",isAuth,sendDeliveryOtp)
 orderRouter.post("/verify-delivery-otp/",verifyDeliveryOtp,)
+
+orderRouter.post("/verify-payment",verifyPayment,)
 
 export default orderRouter
 
