a1bdc966bfbfc32760b7c5f85f4b67bef8629eec
AVIS10060 <avisharmaid@gmail.com>
2026-04-28 23:45:25 +0530

    changes

--- frontend/src/category.js
+++ frontend/src/category.js
@@ -10,10 +10,8 @@ import image9 from "./assets/image9.jpg";
 import image10 from "./assets/image10.avif";
 import image11 from "./assets/image11.jpg";
 
-
-
 export const categories = [
-  { category: "Snacks", image: image1 },
+  { category: "snacks", image: image1 },
   { category: "Main Course", image: image2 },
   { category: "Desserts", image: image3 },
   { category: "Pizza", image: image4 },
@@ -21,7 +19,7 @@ export const categories = [
   { category: "Sandwiches", image: image6 },
   { category: "South Indian", image: image7 },
   { category: "North Indian", image: image8 },
-  { category: "Chinese", image: image9 },
+  { category: "chinese", image: image9 },
   { category: "Fast Food", image: image10 },
-  { category: "All", image: image11 }
+  { category: "others", image: image11 }
 ];
